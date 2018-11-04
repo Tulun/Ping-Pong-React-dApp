87b9665d192cf59b03df4d1ffcde8de1ae457666
Jason Kiraly <jaykiraly@gmail.com>
2018-11-04 08:59:59 -0800

    I can invoke a function call by encoding it with web3.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+
+import EthereumQRPlugin from 'ethereum-qr-code';
+import QRCode from 'qrcode.react';
+
+import leaderboard from './leaderboard';
+const qr = new EthereumQRPlugin()
+const web3 = new Web3(window.web3.currentProvider);
 
 class App extends Component {
+  state = {
+    value: ""
+  };
+  async componentDidMount() {
+    console.log('web3', web3);
+    console.log('lb', leaderboard);
+    console.log(web3.eth.abi.encodeFunctionCall({
+      name: "addPlayerToLeaderboard",
+      type: "function",
+      inputs: [{
+        type: 'string',
+        name: 'name'
+      }]
+    },["Jason"]));
+
+    console.log(await leaderboard.methods.players(0).call())
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Ping Pong Tester</h2>
+        <QRCode id="qrcode" value={this.state.value} />
       </div>
     );
   }
 }
 
 export default App;
+
+    // const qrCode = qr.toCanvas({
+    //   to: "0x9adB410a0Dc71B790dd4e1959658E33e291A714E",
+    //   // from: "0x1d0E501E76Fd7c92fb388053A7424a3ae50e74EC",
+    //   // value: 0, 
+    //   // gas: 55000,
+    //   mode: "contract_function",
+    //   "functionSignature": {
+    //   "name": "addPlayerToLeaderboard",
+    //   "payable": false,
+    //   "args": [
+    //     {
+    //       name: "name",
+    //       type: "string"
+    //     }
+    //   ],
+    // },
+    // "argsDefaults": [
+    //   {
+    //     "name": "name",
+    //     "value": "Jason"
+    //   }
+    // ]
+    // }, {
+    //   selector: "#qrcode"
+    // });
+
+    // qrCode.then(code => {
+    //   console.log('QR code', code, code.value);
+    //   this.setState({ value: code.value })
+    // });
+
+  //   //      "from": "0x1d0E501E76Fd7c92fb388053A7424a3ae50e74EC",
+  //   // ethereum:<address>[?from=<sender_address>][?value=<ethamount>][?gas=<suggestedGas>]
+
+  // const qrString = "ethereum:0x57a0F1cD33d513BdEB47B2C0c1439Bb875135dfD[?from=0x1d0E501E76Fd7c92fb388053A7424a3ae50e74EC][?gas=55000][?args=['Jason']]";
+  //   const testString = `
+  //   {
+  //     "to": "0x57a0F1cD33d513BdEB47B2C0c1439Bb875135dfD",
+  //     "value": 0,
+  //     "gas": 100000,
+  //     "mode": "contract_function",
+  //     "functionSignature": {
+  //       "name": "addPlayerToLeaderboard",
+  //       "payable": false,
+  //       "args": ["Jason"]
+  //     },
+  //   }
+  //   `
